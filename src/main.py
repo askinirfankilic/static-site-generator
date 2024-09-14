1153cc75c42976517290b59b7fb75bbc94683be4
Irfan Askin Kilic <kictakimuhabirsincap@gmail.com>
2024-09-14 20:12:35 +0300

    remove redundant comments

--- src/main.py
+++ src/main.py
@@ -1,12 +1,8 @@
-from src import iohelper
 from src.nodes import nodehelper
 import os
 
 
 def main():
-    public_path = iohelper.get_path_public()
-    static_path = iohelper.get_path_static()
-
     file_path = os.path.dirname(os.path.abspath(__file__))
 
     from_path = "/../content/"
@@ -18,13 +14,7 @@ def main():
     dest_path = "/../public/"
     dest_path = file_path + dest_path
 
-    # iohelper.delete_all_under_directory(public_path)
-    # iohelper.move(static_path, public_path)
-    # nodehelper.generate_page(from_path, template_path, dest_path)
     nodehelper.generate_pages_recursive(from_path, template_path, dest_path)
-
-
-# def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
 
 
 if __name__ == "__main__":
